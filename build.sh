e970d7580d0d9c142bb9f1452d01a4166374e69f
MrBocch <jorgealberto1436@gmail.com>
2025-06-10 12:21:37 -0600

    properly compress the file

--- build.sh
+++ build.sh
@@ -3,9 +3,8 @@
 elm make src/Main.elm --output=static/main.js --optimize # --debug
 
 # Minify the output with UglifyJS
-# this is outdated because its using 'uglifyjs' but its still nice to look at
+# i should pay more attention to the code by the creator of elm
+# rather than chatgpt
 # https://github.com/elm/compiler/blob/9d97114702bf6846cab622a2203f60c2d4ebedf2/hints/optimize.md 
-pnpx uglify-js static/main.js \
-  --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9],pure_getters=true,keep_fargs=false,unsafe_comps=true,unsafe=true" \
-  --mangle \
-  --output static/main.min.js
+
+pnpx uglify-js static/main.js --compress 'pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9,A2,A3,A4,A5,A6,A7,A8,A9],pure_getters,keep_fargs=false,unsafe_comps,unsafe' | pnpx uglify-js --mangle --output static/main.min.js 
