f2a49ab3eefb9b3a407bd1d61c9d3cc8fb64fe58
MrBocch <jorgealberto1436@gmail.com>
2025-06-10 11:57:24 -0600

    correct misspelling and build.sh now also compress the file it compresses it like crazyyyyyyyyyyyyyyyyyyy

--- build.sh
+++ build.sh
@@ -1,3 +1,9 @@
 #!/usr/bin/env zsh
 
 elm make src/Main.elm --output=static/main.js --optimize # --debug
+
+# Minify the output with UglifyJS
+pnpx uglify-js static/main.js \
+  --compress "pure_funcs=[F2,F3,F4,F5,F6,F7,F8,F9],pure_getters=true,keep_fargs=false,unsafe_comps=true,unsafe=true" \
+  --mangle \
+  --output static/main.min.js
